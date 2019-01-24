ca641d2fbaab993996b3613f1a31a859e6178a77
Dmitry Kasnitsky <buildog@mail.ru>
2019-01-24 19:31:06 +0300

    Configuring the project

---
+++ src/client/Components/LeftPanel/LeftPanel.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { MyProps, MyState } from './IScene';
+
+const { PureComponent } = React;
+
+export default class LeftPanel extends PureComponent <MyProps, MyState> {
+  // constructor(props: object) {
+  //   super(props);
+  // }
+
+  componentDidMount() {
+    // fetch('/api/getUsername')
+    //   .then(res => res.json())
+    //   .then(user => this.setState({ username: user.username }));
+  }
+
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
