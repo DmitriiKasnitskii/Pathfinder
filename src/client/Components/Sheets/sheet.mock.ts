6bc859ab5b4dadab5ff407f28ba1253e3dec7dc2
Dmitry Kasnitsky <buildog@mail.ru>
2019-01-28 17:03:02 +0300

    Adding mock object for simple view

---
+++ src/client/Components/Sheets/sheet.mock.ts
@@ -0,0 +1,242 @@
+export default {
+  CharacterName: 'Arf De Kar',
+  PlayerName: 'Kobelev',
+  Deity: 'None',
+  Religion: 'None',
+  Alignment: 'Chaotic Good',
+  Class: 'Sorcerer',
+  Race: 'Aasimar / Outsider',
+  Size: 'Medium / 5ft',
+  Height: "6' 4\"",
+  Vision: 'Darkvision (60 ft.)',
+  CharacterLevel: 1,
+  ExperienceCur: 0,
+  ExperienceNext: 2000,
+  Age: 20,
+  Gender: 'Male',
+  Speed: 'Walk 30 ft.',
+  Eyes: '',
+  Hair: '',
+  Points: 15,
+  StatSheet: {
+    STR: {
+      BaseScore: 14
+    },
+    DEX: {
+      BaseScore: 14
+    },
+    CON: {
+      BaseScore: 10
+    },
+    INT: {
+      BaseScore: 12
+    },
+    WIS: {
+      BaseScore: 10
+    },
+    CHA: {
+      BaseScore: 16
+    },
+    FORT: {
+      Base: 0,
+      Ability: 0,
+      Magic: 0,
+      Misc: 0,
+      Epic: 0,
+      Temp: 0
+    },
+    REFLEX: {
+      Base: 0,
+      Ability: 0,
+      Magic: 0,
+      Misc: 0,
+      Epic: 0,
+      Temp: 0
+    },
+    WILL: {
+      Base: 0,
+      Ability: 0,
+      Magic: 0,
+      Misc: 0,
+      Epic: 0,
+      Temp: 0
+    },
+    SKILLS: {
+      Acrobatics: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      Appraise: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      Bluff: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+      Climb: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'STR'
+      },
+      Craft: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      Diplomacy: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+      DisableDevice: {
+        Untrainder: false,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      Disguise: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+      EscapeArtist: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      Fly: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      HandleAnimal: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+      Heal: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'WIS'
+      },
+      Intimidate: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+      KnowledgeArcana: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeDungeoneering: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeEngineering: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeGeography: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeHistory: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeNature: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeNobility: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgePlanes: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeReligion: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      KnowledgeLocal: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      Linguistic: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      Perception: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'WIS'
+      },
+      Perform: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+      Profession: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'WIS'
+      },
+      Ride: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      SenceMotive: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'WIS'
+      },
+      SleightOfHand: {
+        Untrainder: false,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      Spellcraft: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'INT'
+      },
+      Stealth: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'DEX'
+      },
+      Survival: {
+        Untrainder: true,
+        ArmorPenalty: false,
+        KeyAbility: 'WIS'
+      },
+      Swim: {
+        Untrainder: true,
+        ArmorPenalty: true,
+        KeyAbility: 'STR'
+      },
+      UseMagicDevice: {
+        Untrainder: false,
+        ArmorPenalty: false,
+        KeyAbility: 'CHA'
+      },
+    }
+  }
+};
--- src/client/reducers/sheet.reducer.ts
+++ src/client/reducers/sheet.reducer.ts
@@ -1,23 +1,23 @@
 import sheetActions from '../actions/sheet.actions';
 
-const initState = {a: 1};
+const initState = { a: 1, locale: 'ru' };
 
 export default function sheetReducer(state: any = initState, action: {type: string}) {
-    switch (action.type) {
-        case sheetActions.INCREASE: {
-            return {
-                count: state.count + 1,
-                ...state
-            }
-        }
-        case sheetActions.DECREASE: {
-            return {
-                count: state.count + 1,
-                ...state
-            }
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case sheetActions.INCREASE: {
+      return {
+        count: state.count + 1,
+        ...state
+      };
     }
+    case sheetActions.DECREASE: {
+      return {
+        count: state.count + 1,
+        ...state
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
