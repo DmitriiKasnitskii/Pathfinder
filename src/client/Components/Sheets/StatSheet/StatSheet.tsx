7e975c647c224e78201e8f1e5c3f8ca8997d356d
Dmitry Kasnitsky <buildog@mail.ru>
2019-01-30 00:17:44 +0300

    Added responsible design Added demo of skills table

--- src/client/Components/Sheets/StatSheet/StatSheet.tsx
+++ src/client/Components/Sheets/StatSheet/StatSheet.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { MyProps } from './IStatSheet';
 import './assets/statsheet.scss';
 import TopPart from '../TopPart/TopPart';
+import SkillsTable from './SkillsTable/SkillsTable';
 
 const StatSheet = (props: MyProps) => (
   props.visible && (
     <div className="sheet">
       <TopPart />
+      <SkillsTable />
     </div>
   )
 );
--- src/client/Components/Sheets/sheet.mock.ts
+++ src/client/Components/Sheets/sheet.mock.ts
@@ -19,226 +19,225 @@ export const mainData = {
   Hair: '',
 };
 
+export const skills = {
+  Acrobatics: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  Appraise: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  Bluff: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+  Climb: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'STR'
+  },
+  Craft: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  Diplomacy: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+  DisableDevice: {
+    Untrained: false,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  Disguise: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+  EscapeArtist: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  Fly: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  HandleAnimal: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+  Heal: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'WIS'
+  },
+  Intimidate: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+  KnowledgeArcana: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeDungeoneering: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeEngineering: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeGeography: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeHistory: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeNature: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeNobility: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgePlanes: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeReligion: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  KnowledgeLocal: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  Linguistic: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  Perception: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'WIS'
+  },
+  Perform: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+  Profession: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'WIS'
+  },
+  Ride: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  SenceMotive: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'WIS'
+  },
+  SleightOfHand: {
+    Untrained: false,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  Spellcraft: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'INT'
+  },
+  Stealth: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'DEX'
+  },
+  Survival: {
+    Untrained: true,
+    ArmorPenalty: false,
+    KeyAbility: 'WIS'
+  },
+  Swim: {
+    Untrained: true,
+    ArmorPenalty: true,
+    KeyAbility: 'STR'
+  },
+  UseMagicDevice: {
+    Untrained: false,
+    ArmorPenalty: false,
+    KeyAbility: 'CHA'
+  },
+};
+
 export const stats = {
-  StatSheet: {
-    STR: {
-      BaseScore: 14
-    },
-    DEX: {
-      BaseScore: 14
-    },
-    CON: {
-      BaseScore: 10
-    },
-    INT: {
-      BaseScore: 12
-    },
-    WIS: {
-      BaseScore: 10
-    },
-    CHA: {
-      BaseScore: 16
-    },
-    FORT: {
-      Base: 0,
-      Ability: 0,
-      Magic: 0,
-      Misc: 0,
-      Epic: 0,
-      Temp: 0
-    },
-    REFLEX: {
-      Base: 0,
-      Ability: 0,
-      Magic: 0,
-      Misc: 0,
-      Epic: 0,
-      Temp: 0
-    },
-    WILL: {
-      Base: 0,
-      Ability: 0,
-      Magic: 0,
-      Misc: 0,
-      Epic: 0,
-      Temp: 0
-    },
-    SKILLS: {
-      Acrobatics: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      Appraise: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      Bluff: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-      Climb: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'STR'
-      },
-      Craft: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      Diplomacy: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-      DisableDevice: {
-        Untrainder: false,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      Disguise: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-      EscapeArtist: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      Fly: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      HandleAnimal: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-      Heal: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'WIS'
-      },
-      Intimidate: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-      KnowledgeArcana: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeDungeoneering: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeEngineering: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeGeography: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeHistory: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeNature: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeNobility: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgePlanes: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeReligion: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      KnowledgeLocal: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      Linguistic: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      Perception: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'WIS'
-      },
-      Perform: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-      Profession: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'WIS'
-      },
-      Ride: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      SenceMotive: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'WIS'
-      },
-      SleightOfHand: {
-        Untrainder: false,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      Spellcraft: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'INT'
-      },
-      Stealth: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'DEX'
-      },
-      Survival: {
-        Untrainder: true,
-        ArmorPenalty: false,
-        KeyAbility: 'WIS'
-      },
-      Swim: {
-        Untrainder: true,
-        ArmorPenalty: true,
-        KeyAbility: 'STR'
-      },
-      UseMagicDevice: {
-        Untrainder: false,
-        ArmorPenalty: false,
-        KeyAbility: 'CHA'
-      },
-    }
+  STR: {
+    BaseScore: 14
+  },
+  DEX: {
+    BaseScore: 14
+  },
+  CON: {
+    BaseScore: 10
+  },
+  INT: {
+    BaseScore: 12
+  },
+  WIS: {
+    BaseScore: 10
+  },
+  CHA: {
+    BaseScore: 16
+  },
+  FORT: {
+    Base: 0,
+    Ability: 0,
+    Magic: 0,
+    Misc: 0,
+    Epic: 0,
+    Temp: 0
+  },
+  REFLEX: {
+    Base: 0,
+    Ability: 0,
+    Magic: 0,
+    Misc: 0,
+    Epic: 0,
+    Temp: 0
+  },
+  WILL: {
+    Base: 0,
+    Ability: 0,
+    Magic: 0,
+    Misc: 0,
+    Epic: 0,
+    Temp: 0
   }
 };
